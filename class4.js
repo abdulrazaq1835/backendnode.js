0be775e99d38d4cffff1adbb53b0f34ea6965c4d
abdulrazaq1835 <abdulrazzak1835@gmail.com>
2025-01-19 15:47:31 +0530

    comiiit

--- class4.js
+++
@@ -1,7 +0,0 @@
-console.log(process)
-// console.log(process)
-
-console.log(process.argv[1])
-
-console.log(process.argv)
-console.log(process.argv[2])
---
+++ inpu4.js
@@ -0,0 +1,22 @@
+// console.log(process)
+// console.log(process)
+
+// console.log(process.argv[1])
+
+// console.log(process.argv)
+// console.log(process.argv[2])
+
+const  fs = require('fs')
+
+
+
+console.log(process)
+console.log(process.argv[2])
+
+if (process.argv[2]==='add') {
+
+    fs.writeFileSync(process.argv[3],process.argv[4])
+    
+} else {
+    console.log("plz check your command")
+}
