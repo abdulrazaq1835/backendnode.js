d1c31fd363a92bb2a2d4f0090a4a68fd126600bd
abdulrazaq1835 <abdulrazzak1835@gmail.com>
2025-01-24 12:00:35 +0530

    commit

--- express.js
+++ express.js
@@ -14,4 +14,33 @@ const express = require('express')
     resp.send("welcome to about page  ")
     
   })
-  app.listen(4300)
+
+//   app.listen(4300)
+
+/////////WAY=2............
+
+// app.listen(4200,function(){
+//     console.log("my server is runnnig")
+// })
+
+const server = app.listen(4300,function(){
+
+    const host = server.address().address
+    const port =  server.address().port
+    console.log("my server is runnnig  at http://%s:$s",host,port)
+ 
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
