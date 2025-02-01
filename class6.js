34035348dcc04270ad45c5a0d8a146830c67c880
abdulrazaq1835 <abdulrazzak1835@gmail.com>
2025-02-01 23:50:39 +0530

    commit

--- class6.js
+++ class6.js
@@ -3,7 +3,7 @@ const http =  require('http')
 http.createServer((req ,resp)=>{
     resp.write("abdulrazzak")
 
-    console.log("my server is running")
+    console.log("my server is runningg")
     resp.end()
 
     
