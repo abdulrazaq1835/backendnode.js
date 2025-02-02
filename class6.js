74eeda121b04e0d66dfc12509ddbbe94fa907efc
abdulrazaq1835 <abdulrazzak1835@gmail.com>
2025-02-02 23:14:02 +0530

    coomuit

--- class6.js
+++ class6.js
@@ -3,7 +3,7 @@ const http =  require('http')
 http.createServer((req ,resp)=>{
     resp.write("abdulrazzak")
 
-    console.log("my server is runningg at 4000")
+    console.log("my server is runningg")
     resp.end()
 
     
